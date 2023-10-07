b95814340ba85b749bb36c1fba0adacbb0750f27
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-07 11:11:54 +0200

    AddView - create toolbar button "Save" for saving new Expense Item object
    Expenses
    - add didSet observer to the items  for encoding data into the UserDefaults  and create  custom initializer for decoding data from UserDefaults
    
    ExpesesItem
    - add Codable protocol to the ExpenseItem struct

--- ExpenseTracker/Expenses.swift
+++ ExpenseTracker/Expenses.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+        
+    }
+    
+    init() {
+        if let savadItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savadItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
 }
